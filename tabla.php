4309863958f4a2098c7b10f9c60ead6f5cb7032a
PriscilaTapia <46427698+PriscilaTapia@users.noreply.github.com>
2019-01-08 17:46:18 -0300

    version 1

---
+++ tabla.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+	<div class="container">	
+		<div class="table-responsive-sm">
+		  <table class="table">
+			  <tbody>
+				  <thead>
+				    <tr>
+				      <?php  
+				      	// query
+				      	$sql = "select";
+				      	$query = $pdo->prepare($sql);
+				      	$query->execute();
+
+				      	for($i = 0; $row = $query->fetch(); $i++){
+				      		echo "<th scope='row'>".$i."</th>";
+				      		echo "<th>".$row['rut']."</th>";
+				      		echo "<th>".$row['nombres']."</th>";
+				      		echo "<th>".$row['apell_part']."</th>";
+				      		echo "<th>".$row['apell_mat']."</th>";
+				      	}
+				      ?>
+				    </tr>
+				  </thead>
+			  </tbody>
+		  </table>
+		</div>
+	</div>
+</body>
+</html>
