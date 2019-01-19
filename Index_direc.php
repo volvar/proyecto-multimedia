710dbe1b675913c5c2501130f3ee9e59af755cf4
PriscilaTapia <46427698+PriscilaTapia@users.noreply.github.com>
2019-01-19 18:55:24 -0300

    Pagina principal director

---
+++ Index_direc.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Index direct</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+
+</head>
+<body>
+  <?php include 'menu_direc.php'; ?>
+
+
+</body>
+</html>
--- formulario_.php
+++ formulario_.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Document</title>
+  <title>FORM INGRESO PROTOTIPO</title>
   <link rel="stylesheet" href="css/bootstrap.css">
   <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
   <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
@@ -20,37 +20,8 @@
   </script>
 </head>
 <body>
-  <?php include 'sidebar.php' ?>
+  <?php include 'menu general.php' ?>
   <!-- <menu>menu u otra cosa</menu> -->
-  <div class="container">
-    <div class="row col-md-8"></div>
-    <div class="row col-md-4">
-      <div class="dropdown">
-        <button class="btn btn-default dropdown-toggle" type="button" id="dropdown1" data-toggle="dropdown">
-          Login
-        </button>
-        <div class="dropdown-menu">
-            <form class="pr-4 pl-4">
-                <div class="form-group"> 
-                  <label for="usuario">Usuario:</label>
-                  <input type="text" class="form-control" id="usuario" name="Usuario">
-                </div> 
-                <div class="form-group"> 
-                  <label for="clave">Clave:</label>
-                  <input type="clave" class="form-control" id="clave" name="Clave">
-                </div> 
-                <button type="submit" class="btn btn-primary">Enviar</button>       
-            </form>
-        </div>
-      </div>  
-    </div>
-  </div>
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>    
-
-
-
   <div class="container">
     <div class="row col-md-8">
       <h2>Ingresar Datos</h2>
