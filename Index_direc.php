166dcbbae57589c2ba3cd8dca3250c0ff97961a1
PriscilaTapia <46427698+PriscilaTapia@users.noreply.github.com>
2019-01-21 12:49:30 -0300

    Update Index_direc.php

--- Index_direc.php
+++ Index_direc.php
@@ -2,18 +2,62 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>FORM INGRESO PROTOTIPO</title>
+  <title>INDEX DIRECTOR-SUB...</title>
   <link rel="stylesheet" href="css/bootstrap.css">
 
 <style>
   body{ 
-     padding-top:60px; 
+     padding-top:100px; 
     } 
 </style>
 </head>
 <body>
   <?php include 'menu_direc.php' ?> 
   <div class="container">
+  	<div class="row">	
+		<div class="card-group">
+		  <div class="card border-primary">
+		    <img class="card-img-top" src="img.jpg" alt="Card image cap">
+		    <div class="card-body text-primary text-justify">
+		      <h5 class="card-title">Ingresar</h5>
+		      <ul>
+		      	<li>
+		      		<a class="nav-link" href="Ingresar alumno form.php">Ingresar alumno</a>
+		      	</li>
+		      	<li>
+		      		<a class="nav-link" href="ingresar profesor.php">Ingresar profesor</a>
+		      	</li>
+		      </ul>
+		    </div>
+		  </div>
+		  <div class="card border-secondary">
+		    <img class="card-img-top" src="img_1.jpg" alt="Card image cap">
+		    <div class="card-body text-secondary text-justify">
+		      <h5 class="card-title ">Generar</h5>
+		      <ul>
+		      	<li>
+		      		<a class="nav-link" href="#">Generar pdf</a>
+		      	</li>
+		      	<li>
+		      		<a class="nav-link" href="#">Generar estadisticas</a>
+		      	</li>
+		      </ul>
+		    </div>
+		  </div>
+		  <div class="card border-primary">
+		    <img class="card-img-top" src="img_2.jpg" alt="Card image cap">
+		    <div class="card-body text-primary text-justify">
+		      <h5 class="card-title "> <a class="nav-link" href="#">Obserbaciones</a></h5>
+		    </div>
+		  </div>
+		  <div class="card border-secondary">
+		    <img class="card-img-top" src="img_3.jpg" alt="Card image cap">
+		    <div class="card-body text-secontary text-justify">
+		      <h5 class="card-title ">Horario de salas</h5>
+		    </div>
+		  </div>
+		</div>
+	</div>
 
   </div>
 
