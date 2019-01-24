40a915b5376b7fd22ed27f4bd48f9634ed48c68c
PriscilaTapia <46427698+PriscilaTapia@users.noreply.github.com>
2019-01-24 11:45:10 -0300

    Update session_prueba.php

--- session_prueba.php
+++ session_prueba.php
@@ -1,22 +1,45 @@
 <?php 
-include 'conexion.php'
+	session_start(); // Iniciando sesion
+$error=''; // Variable para almacenar el mensaje de error
+if (isset($_POST['submit'])) {
+if (empty($_POST['username']) || empty($_POST['password'])) {
+$error = "Contraseña y/o usuario incorrecto";
+}
+else
+{
+// Define $username y $password
+$username=$_POST['username'];
+$password=$_POST['password'];
+// Estableciendo la conexion a la base de datos
+// include("config/db.php");//Contienen las variables, el servidor, usuario, contraseña y nombre  de la base de datos
+include("conexion.php");//Contiene de conexion a la base de datos
+ 
+// Para proteger de Inyecciones SQL 
+$username    = mysqli_real_escape_string($con,(strip_tags($username,ENT_QUOTES)));
+$password =  sha1($password);//Algoritmo de encriptacion de la contraseña http://php.net/manual/es/function.sha1.php
+ 
+$sql = "SELECT nombre, clave FROM login WHERE nombre = '" . $username . "' and clave='".$password."';";
+$query=mysqli_query($con,$sql);
+$counter=mysqli_num_rows($query);
+if ($counter==1){
+		$_SESSION['login_user_sys']=$username; // Iniciando la sesion
+		header("location: profile.php"); // Redireccionando a la pagina profile.php
 	
-	$nombre = $_POST['nombre'];
-
-	$rut = $_POST['rut'];
 	
-	session_start();
-	$_SESSION["usuario"] = "$nombre";	
-	$_SESSION["contraseña"] = "$rut";
+} else {
+$error = "Contraseña y/o usuario incorrecto";	
+}
+}
+}
 
 // establecer tiempo de inactividad
-	$inactividad =600;
-	if (isset($_SESSION["timeout"])) {
-		$sessionTTL = time() - $_SESSION["timeout"];
-		if ($sessionTTL > $inactividad) {
-			session_destroy();
-			header("Location: /.php");
-		}
-	}
+	// $inactividad =600;
+	// if (isset($_SESSION["timeout"])) {
+	// 	$sessionTTL = time() - $_SESSION["timeout"];
+	// 	if ($sessionTTL > $inactividad) {
+	// 		session_destroy();
+	// 		header("Location: /.php");
+	// 	}
+	// }
 
  ?>
