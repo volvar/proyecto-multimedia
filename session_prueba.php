a0b04c2b9b6b421712595e7b7c86979d38a1d1b3
PriscilaTapia <46427698+PriscilaTapia@users.noreply.github.com>
2019-01-24 11:05:56 -0300

    Update session_prueba.php

--- session_prueba.php
+++ session_prueba.php
@@ -1,6 +1,25 @@
 <?php 
 include 'conexion.php'
+	
+	$nombre = $_POST['nombre'];
+
+	$rut = $_POST['rut'];
+
+
+	
 	session_start();
-	$_SESSION["usuario"] = 
+	$_SESSION["usuario"] = "$nombre";	
+	$_SESSION["contraseña"] = "$rut";
+
+
+// establecer tiempo de inactividad
+	$inactividad =600;
+	if (isset($_SESSION["timeout"])) {
+		$sessionTTL = time() - $_SESSION["timeout"];
+		if ($sessionTTL > $inactividad) {
+			session_destroy();
+			header("Location: /.php");
+		}
+	}
 
  ?>
