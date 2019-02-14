210e4826dd0613bd6b15177ada53169d64f2c130
PriscilaTapia <46427698+PriscilaTapia@users.noreply.github.com>
2019-02-14 11:42:11 -0300

    ESCANER CODIGO QR
    el archivo esta en docs/1.php

---
+++ ESCANER CODIGO QR/docs/1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>scaner</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+	
+	<?php include ('menu_mayordomo.php') ?>
+	<?php include ('1.html') ?>
+	
+</body>
+</html>
---
+++ ESCANER CODIGO QR/docs/menu_mayordomo.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>menu general</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+  <!-- inicio menu -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <!-- despl Ingresar -->
+      <div class="container">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+          <a class="nav-link" href="#">BITACORA</a>
+        </li>
+        <li class="nav-item active">
+          <a class="nav-link" href="#">ESCANER</a>
+        </li>
+      </ul>
+      </div>
+    </div>
+  </nav>
+<!-- fin menu -->
+</body>
+</html>
